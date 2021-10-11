3da6a0cfa5f949fa613e19a6d8272a76c8753cf9
jeet2610 <jeetshah0@icloud.com>
2021-10-11 12:30:15 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,72 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "",
     title: "JamsInfo",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `My Smart-Home Guide`,
+        short_name: `SH Guide`,
+        description: `Guide for residents of the ABC123 Smart Home`,
+        start_url: `/`,
+        background_color: `#0a68f0`,
+        theme_color: `#0a68f0`,
+        display: `standalone`,
+         icon: `src/images/icon.png`,
+        icons: [
+          {
+            "src": "./src/icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "./src/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable any"
+          }
+        ]
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 };
---
+++ src/pages/internet-issues.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+
+
+
+const IndexPage = () => (
+ 
+   
+    <h1>Internet Issues - Troubleshooting</h1>
+    
+ 
+)
+
+export default IndexPage
