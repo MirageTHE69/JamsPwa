e2d5cbfd0f8f9496a4d189638e2613e13a59c90d
jeet2610 <jeetshah0@icloud.com>
2021-10-11 23:42:42 +0530

    Added Jio UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://relaxed-cray-c503d4.netlify.app/",
+    siteUrl: "https://jeets-blog.netlify.app",
     title: "JamsInfo",
   },
   plugins: [
